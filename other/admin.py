7547db50095997b0c611051949964ac3d1014469
jacky <jackyfeng201314@163.com>
2020-07-11 21:35:24 +0800

    change

--- other/admin.py
+++ other/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Friend
+from .models import Friend, SiteMessage
 # Register your models here.
 admin.site.register(Friend)
+admin.site.register(SiteMessage)
