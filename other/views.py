389e2ca447677ef9bffa8038323563a43c623f9a
jacky <jackyfeng201314@163.com>
2020-07-14 20:27:14 +0800

    view url

--- other/views.py
+++ other/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Friend, SiteMessage
+from .models import Friend, SiteMessage, Timeplan
 # Create your views here.
 def about(request):
     return render(request, "other/about.html")
@@ -11,8 +11,13 @@ def friends(request):
 
 def blog_comment(request):
     return render(request, "other/comment.html")
-
+'''
 def messages(request):
     messages = SiteMessage.objects.all().last()
     contaxt = {"messages" : messages}
     return render(request, "article/list.html",contaxt)
+'''
+def timeplan(request):
+    plans = Timeplan.objects.all()
+    contaxt = {"plans" : plans}
+    return render(request, "other/timeline.html",contaxt)
