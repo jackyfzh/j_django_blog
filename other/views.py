be9bd30dd6f4ac3df98f7a10bfaa76d54f13c3d1
jacky <jackyfeng201314@163.com>
2020-07-14 19:39:17 +0800

    message

--- other/views.py
+++ other/views.py
@@ -13,6 +13,6 @@ def blog_comment(request):
     return render(request, "other/comment.html")
 
 def messages(request):
-    messages = Friend.objects.last()
+    messages = Friend.objects.all().last()
     contaxt = {"ms" : messages}
     return render(request, "article/list.html",contaxt)
